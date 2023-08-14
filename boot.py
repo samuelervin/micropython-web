1dabd5b376903b5409ecb214083596934ddf7284
Sam Ervin <servin2@theark.cloud>
2023-08-14 10:43:33 -0500

    Inital commit.

---
+++ boot.py
@@ -0,0 +1,37 @@
+try:
+  import usocket as socket
+except:
+  import socket
+
+from machine import Pin, Signal
+import network
+import time
+
+import esp
+esp.osdebug(None)
+
+import gc
+gc.collect()
+
+
+ssid = 'Ervin-2g'
+password = 'A10CJ11ASA05BLERVIN'
+#due to a board using an active low signal we will invert from what normal examples show. Check out signal class in micropython
+led_pin = Pin(2, Pin.OUT)
+led = Signal(led_pin,invert=True)
+led.off()
+#create a station
+station = network.WLAN(network.STA_IF)
+#set the station as active
+station.active(True)
+#send connect to the wireless network
+station.connect(ssid, password)
+#this will run until the station is connected
+while station.isconnected() == False:
+  pass
+
+print('Connection successful')
+print(station.ifconfig())
+
+time.sleep(3)
+print("Hello Sam!")
---
+++ main.py
@@ -0,0 +1,93 @@
+led_state = "OFF"
+color = "#000000"
+
+def web_page():
+    html = """<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+     integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <style>
+        html {
+            font-family: Arial;
+            display: inline-block;
+            margin: 0px auto;
+            text-align: center;
+        }
+
+        .button {
+            background-color: #0066ff;
+            border: none;
+            color: white;
+            padding: 16px 40px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 4px 2px;
+            cursor: pointer;
+        }
+
+        .button1 {
+            background-color: #ff6319;
+        }
+    </style>
+</head>
+
+<body>
+    <h2>ESP MicroPython Web Server</h2>
+    <p>LED state: <strong>""" + led_state + """</strong></p>
+    <i class="fas fa-lightbulb fa-3x" style="color:#""" + color + """;"></i>
+    <p>
+        <a href=\"?led_2_on\"><button class="button">LED ON</button></a>
+    </p>
+    <p>
+        <a href=\"?led_2_off\"><button class="button button1">LED OFF</button></a>
+    </p>
+</body>
+
+</html>"""
+    return html
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('', 80))
+s.listen(5)
+
+while True:
+    try:
+        if gc.mem_free() < 102000:
+            gc.collect()
+        conn, addr = s.accept()
+        conn.settimeout(3.0)
+        print('Received HTTP GET connection request from %s' % str(addr))
+        request = conn.recv(1024)
+        conn.settimeout(None)
+        request = str(request)
+        print('GET Rquest Content = %s' % request)
+        
+        led_on = request.find('/?led_2_on')
+        led_off = request.find('/?led_2_off')
+        
+        if led_on == 6:
+            print('LED ON -> GPIO2')
+            led_state = "ON"
+            color = "d8dc55"
+            led.on()
+            
+        if led_off == 6:
+            print('LED OFF -> GPIO2')
+            led_state = "OFF"
+            led.off()
+            color = "000000"
+            
+        response = web_page()
+        conn.send('HTTP/1.1 200 OK\n')
+        conn.send('Content-Type: text/html\n')
+        conn.send('Connection: close\n\n')
+        conn.sendall(response)
+        conn.close()
+    except OSError as e:
+        conn.close()
+        print('Connection closed')
